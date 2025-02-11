a3cd7ce515331302f47c25b0002cd0b3e0321129
JohnSwiftC <johnswift@live.com>
2025-02-10 20:53:25 -0600

    simple https

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
-use tokio::io::AsyncBufRead;
-use tokio::io::AsyncReadExt;
-use tokio::io::AsyncWriteExt;
-use tokio::io::Lines;
-
-use std::net::{TcpListener, TcpStream};
-
-use std::sync::Arc;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+    sync::Arc,
+};
 
 use rustls::{
-    ClientConfig, ClientConnection, CommonState, RootCertStore, ServerConfig, ServerConnection,
-    Stream,
+    pki_types::ServerName, ClientConfig, ClientConnection, CommonState, RootCertStore,
+    ServerConfig, ServerConnection, Stream,
 };
 
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "{}",
+        make_https_request(
+            "www.rust-lang.com".into(),
+            concat!(
+                "GET / HTTP/1.1\r\n",
+                "Host: www.rust-lang.org\r\n",
+                "Connection: close\r\n",
+                "Accept-Encoding: identity\r\n",
+                "\r\n"
+            )
+            .as_bytes(),
+        )
+    );
 }
 
-fn make_request(website: String, request: String) {
+fn make_https_request(website: String, request: &[u8]) -> String {
     let root_store = RootCertStore::from_iter(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
 
     let config = ClientConfig::builder()
@@ -24,11 +34,19 @@ fn make_request(website: String, request: String) {
         .with_no_client_auth();
 
     let rc_config = Arc::new(config);
-    let example_com = "youtube.com".try_into().unwrap();
+    let example_com = ServerName::try_from(website.clone()).expect("Bad DNS");
     let mut client = ClientConnection::new(rc_config, example_com).unwrap();
 
     // We have a TLS 'client connection' object that must be combined with an actual TCP stream on port 443 to be turned into a real stream
 
     let mut sock = TcpStream::connect(&format!("{}:443", website)).unwrap();
     let mut tls_stream = Stream::new(&mut client, &mut sock);
+
+    tls_stream.write_all(request).unwrap();
+
+    let mut response: Vec<_> = Vec::new();
+
+    tls_stream.read_to_end(&mut response).unwrap();
+
+    String::from_utf8(response).expect("Bad Bytes")
 }
